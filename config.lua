e25f2664eb5e95c0ae4a5eb67361c0b3ae7aa7e7
gaurav047 <gauravkumarbtp@gmail.com>
2019-08-19 11:56:07 +0530

    tls-mqtt

---
+++ config.lua
@@ -0,0 +1,14 @@
+-- wifi related
+wifi_ip = "192.168.1.69"
+wifi_netmask ="255.255.255.0"
+wifi_gateway ="192.168.1.1"
+wifi_ssid = "gaurav"
+wifi_pwd = "kumar678"
+-- mqtt
+mqtt_host = "mqtt.googleapis.com"
+mqtt_port = 8883
+mqtt_user = "unused"
+--generate a JWT for the mqtt_password field using the python script
+mqtt_password = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NjYwMzQxNTEsImV4cCI6MTU2NjAzNzc1MSwiYXVkIjoidXBsdWEtMjAxOSJ9.qew2RweD36eKbZVqsWzca714bM15vaHFNy1cwBz8lW62L5lp_olJPhWhwqK4_yDNuQCYvF0J0BkI9ZLzyszWdw"
+mqtt_keepalive = 120
+mqtt_topic_out = "node-out"
